fef6df5934b6e23524ae0210790795c630a6342d
Govind Tripathi <govindtripathi62@gmail.com>
2026-03-23 13:55:52 +0530

    chore: restructured Webion project and initialized ag-kit

--- webion-live/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import LiveShoppingRoom from "@/components/LiveShoppingRoom";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <LiveShoppingRoom />
-    </main>
-  );
-}
